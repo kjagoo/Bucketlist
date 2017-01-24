283256b0346ea56e80091b9827135efa39569469
kagenyi2@gmail.com <kagenyi2@gmail.com>
2017-01-24 19:15:04 +0300

    tests on delete, put , unauthorised access on bucketlist

--- tests/test_app.py
+++ tests/test_app.py
@@ -47,89 +47,84 @@ class TestBucketlists(TestBase):
 
     def test_delete_bucketlist(self):
         """ Test delete bucket lists """
-        response = self.app.delete("/bucketlists/1", headers=self.get_token())
-        print(response)
+        response = self.app.delete("/bucketlists/2864", headers=self.get_token())
         self.assertEqual(response.status_code, 200)
         output = json.loads(response.data.decode('utf-8'))
 
-        self.assertTrue("Successfully deleted bucket list" in output["message"])
-        self.assertTrue("Knowledge Goals" in output["message"])
+        self.assertIn("Successfully deleted bucket list", output["message"])
+        self.assertTrue("2017 December Holiday" in output["message"])
 
     def test_edit_bucketlist(self):
-        """ Test editing of bucket lists """
-        self.bucketlist = {"title": "Mission Multilinguist",
-                           "description": "Languages to learn"}
-        response = self.app.put("/bucketlists/2",
+        """ Test updating of bucket lists """
+        self.bucketlist = {"title": "cooking plans 2017",
+                           "description": "Things i want to cook in 2017"}
+        response = self.app.put("/bucketlists/2837",
                                 data=self.bucketlist,
                                 headers=self.get_token())
         self.assertEqual(response.status_code, 200)
         output = json.loads(response.data.decode('utf-8'))
-        self.assertTrue("You have successfully edited the bucket list"
-                        in output["message"])
+        self.assertIn("Successfully updated bucket list.", output["message"])
         self.assertIn(self.bucketlist["title"], response.data.decode('utf-8'))
         self.assertIn(self.bucketlist["description"], response.data.decode('utf-8'))
 
-    # def test_get_bucketlists(self):
-    #     """ Test that all bucket lists are displayed """
-    #     response = self.app.get("/bucketlists/",
-    #                             headers=self.get_token())
-    #     self.assertEqual(response.status_code, 200)
-    #     output = json.loads(response.data)
-    #     output = output["bucketlists"]
-    #     bucketlist1 = output[0]
-    #     bucketlist2 = output[1]
-    #     # Both bucket lists are displayed
-    #     self.assertEqual(bucketlist1.get("title"), "Knowledge Goals")
-    #     self.assertEqual(bucketlist2.get("title"), "Adventures")
-    #
-    # def test_get_bucketlist(self):
-    #     """ Test that specified bucket list is displayed """
-    #     # Get bucket list whose ID is 1
-    #     response = self.app.get("/bucketlists/1",
-    #                             headers=self.get_token())
-    #     self.assertEqual(response.status_code, 200)
-    #     bucketlist1 = json.loads(response.data)
-    #     self.assertEqual(bucketlist1.get("title"), "Knowledge Goals")
-    #
-    #     # Get bucket list whose ID is 2
-    #     response = self.app.get("/bucketlists/2",
-    #                             headers=self.get_token())
-    #     self.assertEqual(response.status_code, 200)
-    #     bucketlist1 = json.loads(response.data)
-    #     self.assertEqual(bucketlist1.get("title"), "Adventures")
-    #
-    # def test_get_nonexistent_bucketlist(self):
-    #     """
-    #     Test that specifying a bucket list with invalid id
-    #     will throw an error
-    #     """
-    #     response = self.app.get("/bucketlists/200",
-    #                             headers=self.get_token())
-    #     self.assertEqual(response.status_code, 403)
-    #     output = json.loads(response.data)
-    #     self.assertTrue("The bucket list specified doesn't exist. "
-    #                     "Please try again!" in output["message"])
-    #
-    # def test_unauthorized_access(self):
-    #     """ Test that users cannot access another user's bucket lists """
-    #     # Register a new user and obtain their token
-    #     self.user = {"username": "testuser3", "password": "testpassword"}
-    #     response = self.app.post("/auth/register/", data=self.user)
-    #     response = self.app.post("/auth/login/", data=self.user)
-    #     output = json.loads(response.data.decode('utf-8'))
-    #     token = output.get("token").encode("ascii")
-    #     token = {"token": token}
-    #
-    #     # No bucket lists are displayed
-    #     response = self.app.get("/bucketlists/", headers=token)
-    #
-    #     self.assertEqual(response.status_code, 200)
-    #     output = json.loads(response.data)
-    #     self.assertTrue("Bucket Lists Empty" in output["message"])
-    #
-    #     # Attempt to get another user's bucket list
-    #     response = self.app.get("/bucketlists/1", headers=token)
-    #     self.assertEqual(response.status_code, 403)
-    #     output = json.loads(response.data)
-    #     self.assertTrue("Error: You are not authorized to access this resource"
-    #                     in output["message"])
+    def test_get_bucketlists(self):
+        """ Test that all bucket lists are displayed """
+        response = self.app.get("/bucketlists/",
+                                headers=self.get_token())
+        self.assertEqual(response.status_code, 200)
+        output = json.loads(response.data.decode('utf-8'))
+        output = output["bucketlists"]
+        bucketlist1 = output[0]
+        bucketlist2 = output[1]
+        # bucket lists are displayed
+        self.assertEqual(bucketlist1.get("title"), "2017 December Holiday")
+        #self.assertEqual(bucketlist2.get("title"), "cooking plans 2017")
+
+    def test_get_bucketlist_id(self):
+        """ Test that specified ID bucket list is displayed """
+        # Get bucket list whose ID is 2837
+        response = self.app.get("/bucketlists/2837",
+                                headers=self.get_token())
+        self.assertEqual(response.status_code, 200)
+        bucketlist1 = json.loads(response.data.decode('utf-8'))
+        self.assertEqual(bucketlist1.get("title"), "cooking plans 2017")
+
+        # Get bucket list whose ID is 2732
+        response = self.app.get("/bucketlists/2783",
+                                headers=self.get_token())
+        self.assertEqual(response.status_code, 200)
+        bucketlist1 = json.loads(response.data.decode('utf-8'))
+        self.assertEqual(bucketlist1.get("title"), "2017 December Holiday")
+
+    def test_get_invalid_bucketlist_id(self):
+        """ Test error raised on invalid bucketlist ID """
+        response = self.app.get("/bucketlists/200",
+                                headers=self.get_token())
+        self.assertEqual(response.status_code, 403)
+        output = json.loads(response.data.decode('utf-8'))
+        self.assertIn("Error: The bucket list doesn't exist.", output["message"])
+
+    def test_unauthorized_access(self):
+        """ Test that users cannot access another user's bucket lists """
+        # Register a new user and obtain their token
+        self.user = {"username": "testuser3", "password": "testpassword",
+                     "email": "test@test.com"}
+        response = self.app.post("/auth/register/", data=self.user)
+        response = self.app.post("/auth/login/", data=self.user)
+        output = json.loads(response.data.decode('utf-8'))
+        token = output.get("token").encode("ascii")
+        token = {"token": token}
+
+        # No bucket lists are displayed
+        response = self.app.get("/bucketlists/", headers=token)
+
+        self.assertEqual(response.status_code, 200)
+        output = json.loads(response.data.decode('utf-8'))
+        self.assertTrue("Bucket Lists Empty" in output["message"])
+
+        # Attempt to get another user's bucket list
+        response = self.app.get("/bucketlists/2837", headers=token)
+        self.assertEqual(response.status_code, 403)
+        output = json.loads(response.data.decode('utf-8'))
+        self.assertIn("Error: Restricted unauthorised access.",
+                        output["message"])
